c8faf4d5700093eb84ed717bbb0b4529b29bf905
Jeff Hollan <jehollan@microsoft.com>
2018-07-11 14:46:25 -0700

    initial

---
+++ HttpTriggerJS/index.js
@@ -0,0 +1,11 @@
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+
+    context.res = {
+        status: 200,
+        body: "Hello"
+    };
+    
+    context.done();
+};
